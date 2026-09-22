77afb22a044e765f8cf1db7ee9031c72272651b4
Raphael Carassou <raphaelcarassou7@gmail.com>
2026-09-22 16:04:04 +0400

    Keran a merdé

---
+++ Assets/Scripts/Serp.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Serp : MonoBehaviour
+{   // Les GameObject apparait dans l'éditeur
+    public GameObject A;
+    public GameObject B;
+    public GameObject C;
+    public GameObject miDistance;
+    public GameObject origine;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {  // Je choisi entre A, B et C mon préféré
+        origine = A;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            if (origine = A)
+            {
+
+                int rng = Random.Range(0, 2);
+                //si mon random est 0 je choisi B sinon je choisi C
+            }
+
+
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+    
+
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,25 +2,51 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject mob;
+    public GameObject sol;
+    public GameObject mur;
+    public int RandomSol;
     public KeyCode spawn = KeyCode.E;
-
+    public int compteur = 0;
     void Start()
     {
-       
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.E))
+        for (int i = 0; i < 100; i++)
         {
-            Instantiate(mob, new Vector3(1, 1, 1), Quaternion.identity);
-            Instantiate(mob, new Vector3(2, 1, 1), Quaternion.identity);
-            Instantiate(mob, new Vector3(3, 1, 1), Quaternion.identity);
+            for (int j = 0; j < 100; j++)
+            {
+                int rng = Random.Range(0, 10);
+                if (rng < 8)
+                {
+                    
+
+                    if (i == 0 || j == 0 || i == 99 || j == 99)
+                    {
+                        Instantiate(sol, new Vector3(i, 1, j), Quaternion.identity);
+                        Instantiate(sol, new Vector3(i, 2, j), Quaternion.identity);
+
+                            if (i == 0 && j == 0 || i == 0 && j == 99 || i == 99 && j == 99 || i == 99 && j == 0)
+                        {
+                            for (int k = 0; k < 100; k++)
+                            {
+                                Instantiate(sol, new Vector3(i, k, j), Quaternion.identity);
+                            }
+                        }
+                    }
+                    else Instantiate(sol, new Vector3(i, 0, j), Quaternion.identity);
+
+                }
+
+
+            }
+
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
         }
 
     }
-
 }
